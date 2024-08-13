4065696ed87b71f0377244f4beabba145ee92daf
Siddarth <siddarth_ch@outlook.com>
2024-08-13 09:33:37 -0700

    added random 3d object for test

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 import * as THREE from "three";
+import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(
@@ -7,28 +8,37 @@ const camera = new THREE.PerspectiveCamera(
   0.1,
   1000
 );
+camera.position.z = 40;
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.setAnimationLoop(animate);
-document.getElementById("here").appendChild(renderer.domElement);
+renderer.setClearColor(0xadd8e6, 1);
+document.getElementById("sceneSpace").appendChild(renderer.domElement);
 
-const light = new THREE.PointLight(0xffffff, 100, 100);
+const light = new THREE.AmbientLight(0xffffff, 10, 100);
 light.position.set(10, 10, 10);
 scene.add(light);
 
-const geometry = new THREE.BoxGeometry(10, 10, 10);
-const material = new THREE.MeshStandardMaterial({
-  color: 0x00ff83,
+const loader = new GLTFLoader().setPath("/clothes_for_expeditions/");
+loader.load("scene.gltf", (gltf) => {
+  const mesh = gltf.scene;
+  mesh.position.set(0, -10, 0);
+  scene.add(mesh);
 });
-const sphere = new THREE.Mesh(geometry, material);
-scene.add(sphere);
 
-camera.position.z = 40;
+let mouseX = 0;
+let mouseY = 0;
+
+document.addEventListener("mousemove", function (e) {
+  mouseX = (e.clientX - window.innerWidth / 2) / 100;
+  mouseY = (e.clientY - window.innerHeight / 2) / 100;
+});
 
 function animate() {
-  sphere.rotation.x += 0.01;
-  sphere.rotation.y += 0.01;
+  camera.position.x += (mouseX - camera.position.x) * 0.1;
+  camera.position.y += (mouseY - camera.position.y) * 0.1;
+  camera.lookAt(scene.position);
 
   renderer.render(scene, camera);
 }
