f852ce9ab0ab10ab7e01c34375d862314199a85a
Siddarth <135114762+PearlD31@users.noreply.github.com>
2024-09-19 20:14:54 -0700

    added config file

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,17 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  async headers() {
+    return [
+      {
+        source: "/:path*{/}?",
+        headers: [
+          {
+            key: "X-Accel-Buffering",
+            value: "no",
+          },
+        ],
+      },
+    ];
+  },
 });
