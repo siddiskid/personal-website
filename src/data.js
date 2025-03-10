eb4a225a2e387ea75505e30679af63d2ef28b246
Siddarth <schiluku@student.ubc.ca>
2025-03-10 06:46:57 -0700

    added projects

--- src/data.js
+++ src/data.js
@@ -1,37 +1,44 @@
 export const projList = {
+  Prescripto: [
+    "https://github.com/siddiskid/Prescripto",
+    "",
+    ["Python", "JS", "React", "Firebase", "Gemini", "OpenAI"],
+  ],
   "Stock Price Predictor": [
     "https://github.com/siddiskid/stock_price_predictior",
     "",
-    ["Python", "PyTorch", "SKLearn", "JS", "React"]
+    ["Python", "PyTorch", "SKLearn", "JS", "React"],
   ],
-  AI: [
-    "https://github.com/siddiskid/AI",
+  Makemore: [
+    "https://github.com/siddiskid/AI/tree/main/makemore",
     "",
-    ["Python", "PyTorch", "Matplotlib"]
+    ["Python", "PyTorch"],
+  ],
+  AI: ["https://github.com/siddiskid/AI", "", ["Python", "PyTorch"]],
+  "WIDS Datathon": [
+    "https://github.com/amahaj02/WIDS-Case-Comp",
+    "",
+    ["Python", "R", "Scikit-learn", "PyTorch"],
   ],
   micrograd_plus: [
     "https://github.com/siddiskid/micrograd_plus",
-    "Inspired by Andrej Karpathy's micrograd, micrograd_plus is an autograd engine over scalar-valued Directed Acyclic Graphs. It extends micrograd by implementing additional activation functions, like sigmoid and relu, in addition to the included tanh along with a draw function that draws a DAG with all neurons and their gradients. micrograd_plus can be used to make neural networks for binary classification tasks",
+    "",
     ["Python", "PyTorch"],
   ],
   worklearn_scraper: [
     "https://github.com/siddiskid/worklearn_scraper",
-    "A web scraping application used to get job listing data from careers online on an excel sheet. Built to save time on reading job requirements and spend that on actually applying to jobs",
+    "",
     ["Python", "Selenium", "Pandas"],
   ],
   "Ryu's Conquest": [
     "https://github.com/siddiskid/Snakes-and-Hackers-II",
-    "Once upon a time, a village is invaded by an army of malevolent robots. Upon leaving, they took away the most valuable things in the world — games. While the inhabitants of the village were filled with sadness, a brave warrior named Ryu has decided to step up and go on a quest to bring back the games. To do that, he needs to beat the robots in four classic games on planet Xamos, Opus, Icrozar, and Duex. Let’s see if he can beat the robots!",
+    "",
     ["JS", "React"],
   ],
-  "Budget Buddy": [
-    "https://github.com/siddiskid/budget_buddy",
-    "Budget Buddy is a dynamic budget tracking app designed to make managing your finances a breeze. With insights about your spending habits, monthly and weekly statistics, and ability to set realistic budget limits, Budget Buddy helps you develop healthy financial habits and makes adulting a little bit easier",
-    ["Java"],
-  ],
+  "Budget Buddy": ["https://github.com/siddiskid/budget_buddy", "", ["Java"]],
   "Wellness 360": [
     "https://github.com/siddiskid/Wellness360",
-    "Wellness 360 is a website which shows users the nearest health and lifestyle locations and lets them book appointments with any of these places. It also has a chatbot which asks the user what symptoms they are facing and tells them the disease they are most likely affected by. Additionally, Wellness 360 Enterprise is a website for businesses, where they can register themselves with us and get updates on appointments booked with them.",
+    "",
     ["JS", "React"],
   ],
 };
@@ -52,47 +59,40 @@ export const logoLookup = {
   Firebase: "icons/FirebaseLogo.png",
   ThreeJS: "icons/ThreeJSLogo.png",
   Flutter: "icons/FlutterLogo.png",
-  "C++": "https://storage.cloud.google.com/coolest_images_bucket/C%2B%2BLogo.png",
+  "C++":
+    "https://storage.cloud.google.com/coolest_images_bucket/C%2B%2BLogo.png",
   C: "icons/CLogo.png",
   Java: "icons/JavaLogo.webp",
   R: "icons/RLogo.png",
   Matlab: "icons/MatlabLogo.png",
+  Blender: "icons/BlenderLogo.png",
+  Unity: "icons/UnityLogo.png",
+  Gemini: "icons/GeminiLogo.avif",
+  OpenAI: "icons/OpenAILogo.png",
 };
 
 export const techStack = {
   Python: [1, "icons/PythonLogo.svg"],
   PyTorch: [2, "icons/PyTorchLogo.png"],
   "scikit-learn": [3, "icons/SKLearnLogo.svg"],
-  Tensorflow: [
-    4,
-    "icons/TensorflowLogo.webp",
-  ],
+  Tensorflow: [4, "icons/TensorflowLogo.webp"],
   Numpy: [5, "icons/NumpyLogo.svg"],
-  Pandas: [6, "icons/PandasLogo.svg"],
-  Matplotlib: [
-    7,
-    "icons/MatplotlibLogo.png",
-  ],
-  Selenium: [8, "icons/SeleniumLogo.png"],
-  JavaScript: [9, "icons/JSLogo.png"],
-  React: [10, "icons/ReactLogo.svg"],
-  MongoDB: [11, "icons/MongoDBLogo.svg"],
-  ExpressJS: [
-    12,
-    "icons/ExpressJSLogo.png",
-  ],
-  Firebase: [
-    13,
-    "icons/FirebaseLogo.png",
-  ],
-  ThreeJS: [14, "icons/ThreeJSLogo.png"],
-  Node: [15, "icons/NodeLogo.png"],
-  Flutter: [16, "icons/FlutterLogo.png"],
-  "C++": [17, "icons/C++Logo.png"],
-  C: [18, "icons/CLogo.png"],
-  Java: [19, "icons/JavaLogo.webp"],
-  R: [20, "icons/RLogo.png"],
-  Matlab: [21, "icons/MatlabLogo.png"],
+  Selenium: [6, "icons/SeleniumLogo.png"],
+  JavaScript: [7, "icons/JSLogo.png"],
+  React: [8, "icons/ReactLogo.svg"],
+  MongoDB: [9, "icons/MongoDBLogo.svg"],
+  ExpressJS: [10, "icons/ExpressJSLogo.png"],
+  Firebase: [11, "icons/FirebaseLogo.png"],
+  ThreeJS: [12, "icons/ThreeJSLogo.png"],
+  Node: [13, "icons/NodeLogo.png"],
+  Flutter: [14, "icons/FlutterLogo.png"],
+  "C++": [15, "icons/C++Logo.png"],
+  C: [16, "icons/CLogo.png"],
+  Java: [17, "icons/JavaLogo.webp"],
+  R: [18, "icons/RLogo.png"],
+  Matlab: [19, "icons/MatlabLogo.png"],
+  Blender: [20, "icons/BlenderLogo.png"],
+  Unity: [21, "icons/UnityLogo.png"],
 };
 
 export const contactLogos = {
